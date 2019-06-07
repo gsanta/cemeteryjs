626a9de68a76159b4f89d86c30695880b4fe2018
gsanta <gsanta@chemaxon.com>
2019-06-07 22:06:52 +0200

    Sketching algorithm to change border item polygons to lines correctly.

--- src/transformators/BorderItemsToLinesTransformator.ts
+++ src/transformators/BorderItemsToLinesTransformator.ts
@@ -51,3 +51,49 @@ export class BorderItemsToLinesTransformator implements WorldItemTransformator {
         return (<Polygon>shape).stretch(this.scales.xScale / 2, this.scales.yScale / 2);
     }
 }
+
+/**
+ *
+ *
+ * for each room do the following
+ *    for each side Segment of the actual room do the following
+ *        1. stretch the given side Segment of the room
+ *
+ *        if wall(s) Segment(s) belonging to that room side Segments are not marked as visited (they should be all marked or none of them)
+ *            stretch the wall(s) Segment(s) belonging to that room side Segment
+ *                (there can be multiple wall Segments that belong to that room side so stretch them in the following way:)
+ *            2. make all of the wall(s) Segment(s) bigger by the same ratio the room side Segment was increased
+ *            3. reposition the wall(s) Segment(s) starting at one endpoint of the new room side Segment and putting them one after the other
+ *
+ *        4. connect the left room side Segment to the left endpoint of the stretched room side Segment
+ *
+ *        if wall(s) Segment(s) belonging to the left room side Segment are not marked as visited (they should be all marked or none of them)
+ *           5. get the angle by which the left neighbouring Segment was rotated
+ *           6. get the ratio by which the neighbouring Segment's length changed
+ *           7. rotate the wall(s) Segment(s) belonging to the left room Segment at the stable endpoint of the room Segment as the pivot point
+ *           8. change the wall(s) Segments length by the ratio calculated at 5.
+ *           9. reposition the wall(s) Segment(s) starting at one endpoint of the new room side Segment and putting them one after the other
+ *           10. go back to step 3. and do the same to the right side of the room Side segment which was stretched
+ *
+ *
+ * 1: Segment.stretch by wall Segments width
+ * 2: Segment.stretch for all Segments
+ * 3:
+ *      - choose one arbitrary end point of the new room Segment
+ *      - sort the wall Segments by asc order based on their distance to this room Segment endPoint (use Point.distanceTo)
+ *      - init referencePoint as the room Segment endpoint
+ *      - iterate over all of the wall Segments
+ *          - get the closer endPoint of the wall Segment to the reference Point
+ *          - subtract referencePoint from the chosen wall Segment endPoint to get a Vector
+ *          - add the Vector to both pf the endPoints of the wall Segment
+ *          - set the other endPoint of the wall Segment (not the one which was chosen previously) as the referencePoint
+ * 4:
+ *  - get the left room side Segment
+ *  - get the Segment's endPoint which is closer to the original (before stretching) wall Segments left endPoint
+ *  - change that endPoint to the stretched left endPoint of the Segment
+ *  - calculate the rotation angle between the original end the modified Segment
+ *
+ * 3: - use Point.distanceTo to get the distance for all wall Segments to one of the new endpoints of the room side Segment
+ *    - use Line.getSegmentWithCenterPointAndDistance
+ *
+ */
