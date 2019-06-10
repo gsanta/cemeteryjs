beb01b409c15ca535d9af7abfefbc4bf79d453f4
gsanta <gsanta@chemaxon.com>
2019-06-10 22:15:31 +0200

    Better algorithm to convert border items to lines.

--- src/transformators/BorderItemsToLinesTransformator.test.ts
+++ src/transformators/BorderItemsToLinesTransformator.test.ts
@@ -7,6 +7,7 @@ import { ScalingTransformator } from './ScalingTransformator';
 import { PolygonAreaInfoParser } from '../parsers/polygon_area_parser/PolygonAreaInfoParser';
 import { Polygon, Point } from '@nightshifts.inc/geometry';
 import { WorldItemInfoFactory } from '../WorldItemInfoFactory';
+import { WorldItemInfo } from '../WorldItemInfo';
 
 
 describe('`BorderItemsToLinesTransformator`', () => {
@@ -123,6 +124,65 @@ describe('`BorderItemsToLinesTransformator`', () => {
                 new Polygon([new Point(0.5, 0.5), new Point(6.5, 0.5), new Point(6.5, 6.5), new Point(3.5, 6.5), new Point(3.5, 3.5), new Point(0.5, 3.5)])
             );
             expect(items[1].dimensions).to.eql(new Polygon([new Point(0.5, 3.5), new Point(3.5, 3.5), new Point(3.5, 6.5), new Point(0.5, 6.5)]));
+        });
+    });
+
+    describe('runAlgorithm', () => {
+        it.only ('runs the algorithm', () => {
+            const polygon = new Polygon([
+                new Point(1, 1),
+                new Point(1, 3),
+                new Point(5, 3),
+                new Point(5, 1)
+            ]);
+
+            const polygon2 = new Polygon([
+                new Point(6, 1),
+                new Point(6, 3),
+                new Point(8, 3),
+                new Point(8, 1)
+            ]);
+
+            const worldItemInfo = new WorldItemInfo(1, 'room', polygon, 'room');
+            const worldItemInfo2 = new WorldItemInfo(2, 'room', polygon2, 'room');
+
+            const borderItemsToLinesTransformator = new BorderItemsToLinesTransformator();
+            const newPolygons = borderItemsToLinesTransformator.runAlgorithm([worldItemInfo, worldItemInfo2]);
+        });
+    });
+
+    describe('alignBorderItems', () => {
+        it.only ('works', () => {
+            const borderItems = [
+                new WorldItemInfo(1, 'wall', new Polygon([new Point(1, 1), new Point(1, 5), new Point(2, 5), new Point(2, 1)]), 'wall'),
+                new WorldItemInfo(2, 'wall', new Polygon([new Point(2, 4), new Point(2, 5), new Point(6, 5), new Point(6, 4)]), 'wall'),
+                new WorldItemInfo(3, 'wall', new Polygon([new Point(6, 1), new Point(6, 5), new Point(7, 5), new Point(7, 1)]), 'wall'),
+                new WorldItemInfo(4, 'wall', new Polygon([new Point(2, 1), new Point(2, 2), new Point(6, 2), new Point(6, 1)]), 'wall')
+            ];
+
+            const poly1 = new Polygon([
+                new Point(2, 2),
+                new Point(2, 4),
+                new Point(6, 4),
+                new Point(6, 2)
+            ]);
+
+            const poly2 = new Polygon([
+                new Point(1, 1),
+                new Point(1, 5),
+                new Point(7, 5),
+                new Point(7, 1)
+            ]);
+
+            const borderItemsToLinesTransformator = new BorderItemsToLinesTransformator();
+            debugger;
+            const newPolygons = borderItemsToLinesTransformator.alignBorderItems(
+                borderItems,
+                poly1,
+                poly2
+            );
+            1;
+            debugger;
         });
     });
 });
--- src/transformators/BorderItemsToLinesTransformator.ts
+++ src/transformators/BorderItemsToLinesTransformator.ts
@@ -1,8 +1,9 @@
 import { WorldItemInfo } from "../WorldItemInfo";
 import { WorldItemTransformator } from './WorldItemTransformator';
-import { Polygon, Line, Shape } from '@nightshifts.inc/geometry';
+import { Polygon, Line, Shape, Point } from '@nightshifts.inc/geometry';
 import { WorldItemInfoUtils } from '../WorldItemInfoUtils';
 import { Segment } from '@nightshifts.inc/geometry/build/shapes/Segment';
+import _ = require("lodash");
 
 /**
  * It can be useful to represent walls, doors etc. as `Segment`s instead of `Polygon`s, so this class transforms the border item
@@ -44,11 +45,84 @@ export class BorderItemsToLinesTransformator implements WorldItemTransformator {
 
         const segment = new Segment(point1, point2);
 
-        return segment.shorten(perpendicularSegments.length)
+        // return segment.shorten(perpendicularSegments.length)
+        return null;
     }
 
     private expandRoomShapeToFillFreedUpSpace(shape: Shape): Shape {
         return (<Polygon>shape).stretch(this.scales.xScale / 2, this.scales.yScale / 2);
+    }
+
+    public runAlgorithm(rooms: WorldItemInfo[]) {
+        return rooms.map((room: WorldItemInfo) => {
+            const segments = room.dimensions.getEdges();
+            const newSegments: Segment[] = [];
+
+            segments.forEach((segment: Segment) => {
+                const centerPoint = segment.getBoundingCenter();
+                const perpendicularBisector = segment.getPerpendicularBisector();
+                const tmpSegment = perpendicularBisector.getSegmentWithCenterPointAndDistance(centerPoint, 0.5);
+                const roomCenterPoint = room.dimensions.getBoundingCenter();
+                let chosenEndPoint: Point = null;
+                if (tmpSegment[0].distanceTo(roomCenterPoint) > tmpSegment[1].distanceTo(roomCenterPoint)) {
+                    chosenEndPoint = tmpSegment[0];
+                } else {
+                    chosenEndPoint = tmpSegment[1];
+                }
+
+                const line = Line.createFromPointSlopeForm(chosenEndPoint, segment.getSlope());
+                const points = line.getSegmentWithCenterPointAndDistance(chosenEndPoint, segment.getLength() / 2 + 0.5);
+                const newSegment = new Segment(points[0], points[1]);
+                newSegments.push(newSegment);
+            });
+
+            const newPoints: Point[] = [];
+            newPoints.push(newSegments[0].points[0]);
+            newPoints.push(newSegments[0].points[1]);
+            _.without(newSegments, newSegments[0], _.last(newSegments)).forEach(segment => {
+                if (segment.points[0].equalTo(_.last(newPoints))) {
+                    newPoints.push(segment.points[1]);
+                } else {
+                    newPoints.push(segment.points[0]);
+                }
+            });
+
+            return new Polygon(newPoints);
+        });
+    }
+
+    public alignBorderItems(borderItems: WorldItemInfo[], dimensions: Polygon, newDimensions: Polygon) {
+        const oldEdges = dimensions.getEdges();
+        const newEdges = newDimensions.getEdges();
+
+        newEdges.forEach((edge, index) => {
+            const coincidentBorderItems = borderItems.filter(item => item.dimensions.getCoincidentLineSegment(oldEdges[index]));
+
+            const maxLen = coincidentBorderItems
+                .map(borderItem => borderItem.dimensions.getCoincidentLineSegment(oldEdges[index])[0])
+                .reduce((sum, nextItem) => sum + nextItem.getLength(), 0);
+
+            coincidentBorderItems.sort((item1, item2) => {
+                return item1.dimensions.getBoundingCenter().distanceTo(edge.points[0]) - item2.dimensions.getBoundingCenter().distanceTo(edge.points[0])
+            });
+
+            const referencePoint = edge.points[0];
+
+            coincidentBorderItems.forEach(item => {
+                const len = newEdges[index].getLength();
+                const ratio = item.dimensions.getCoincidentLineSegment(oldEdges[index])[0].getLength() / maxLen;
+                const line = Line.createFromPointSlopeForm(referencePoint, edge.getSlope());
+                const tmpSegment = line.getSegmentWithCenterPointAndDistance(referencePoint, (ratio * len) / 2);
+
+                if (tmpSegment[0].distanceTo(edge.points[1]) < tmpSegment[1].distanceTo(edge.points[1])) {
+                    const newSegment = line.getSegmentWithCenterPointAndDistance(tmpSegment[0], (ratio * len) / 2);
+                    item.dimensions = new Segment(newSegment[0], newSegment[1]);
+                } else {
+                    const newSegment = line.getSegmentWithCenterPointAndDistance(tmpSegment[1], (ratio * len) / 2);
+                    item.dimensions = new Segment(newSegment[0], newSegment[1]);
+                }
+            });
+        });
     }
 }
 
@@ -76,24 +150,55 @@ export class BorderItemsToLinesTransformator implements WorldItemTransformator {
  *           10. go back to step 3. and do the same to the right side of the room Side segment which was stretched
  *
  *
- * 1: Segment.stretch by wall Segments width
- * 2: Segment.stretch for all Segments
- * 3:
- *      - choose one arbitrary end point of the new room Segment
- *      - sort the wall Segments by asc order based on their distance to this room Segment endPoint (use Point.distanceTo)
- *      - init referencePoint as the room Segment endpoint
- *      - iterate over all of the wall Segments
- *          - get the closer endPoint of the wall Segment to the reference Point
- *          - subtract referencePoint from the chosen wall Segment endPoint to get a Vector
- *          - add the Vector to both pf the endPoints of the wall Segment
- *          - set the other endPoint of the wall Segment (not the one which was chosen previously) as the referencePoint
+ *  iterate over all of the rooms
+ *        iterate over all of the room side Segments
+ *            get the corresponding wall Segments to the current room side Segment (use Shape.getCoincidentLineSegment to filter the relevant wall Segments)
+ *            get the width on of the wall segments (each should have the same width)
+ *            stretch the room side Segment by that width (use Segment.stretch)
+ *            stretch the corresponding wall side Segment(s) (use Segment.stretch)
+ *                - choose one arbitrary end point of the new room Segment
+ *                - sort the wall Segments by asc order based on their distance to this room Segment endPoint (use Point.distanceTo)
+ *                - init referencePoint as the room Segment endpoint
+ *                - iterate over all of the wall Segments
+ *                    - get the closer endPoint of the wall Segment to the reference Point
+ *                    - subtract referencePoint from the chosen wall Segment endPoint to get a Vector
+ *                    - add the Vector to both pf the endPoints of the wall Segment
+ *                    - set the other endPoint of the wall Segment (not the one which was chosen previously) as the referencePoint
  * 4:
- *  - get the left room side Segment
- *  - get the Segment's endPoint which is closer to the original (before stretching) wall Segments left endPoint
- *  - change that endPoint to the stretched left endPoint of the Segment
- *  - calculate the rotation angle between the original end the modified Segment
+ *  -           get the left room side Segment
+ *  -           get the Segment's endPoint which is closer to the original (before stretching) room side Segments left endPoint
+ *  -           change that endPoint to the stretched left endPoint of the Segment
  *
- * 3: - use Point.distanceTo to get the distance for all wall Segments to one of the new endpoints of the room side Segment
- *    - use Line.getSegmentWithCenterPointAndDistance
+ * 5.           use Point.angleTo with the original room side Segment left endpoint and the modified left endpoint to get the rotation angle
+ * 6.           use the original left room side Segment and the new one to calculate the length change: newLength / origLength
+ * 7.           get the wall Segment(s) belonging to the left room side Segment
+ *              calculate the wall Segment(s) length ratio to the  left room side segment, order them by their distance to the stable room side Segment endpoint
+ *              set the stable endpoint of the left room side Segment as the referencePoint
+ *              iterate over the wall Segments
+ *                calc radius for the new wall side Segment as the half of the ratio * new left room side Segment length
+ *                create a temporary Segment with the referencePoint as center and with the previously calculated radius
+ *                from the two endPoints of the temporary Segment choose the one which is closer to the rotated endPoint of the left
+ *                room side Segment
+ *                create a new wall Segment with the previously chosen point as center point and the radius as radius
+ *                again get the endPoint of the new wall Segment which is closer to the rotated left room side Segment
+ *                set this point as the next reference point
  *
+ *
+ * *************************************************************8
+ *
+ * store wall side width
+ * iterate over all of the rooms
+ *     iterate over all of the room side Segments
+ *         if act room side Segment was not visited
+ *             stretch the room side Segment by half wall side width on both directions
+ *             move room side Segment to it's normal direction by half wall side width
+ *             get wall side Segments which coincide to the original pos of the act room side Segment
+ *             get the size ratios of the wall side Segment(s) to the original room side Segment
+ *             order wall side Segment(s) by their distance to one of the original endpoints of the room side Segment
+ *             set the chosen original endpoint as the referencePoint
+ *             iterate over the wall side Segment(s)
+ *                 create a temporary Segment with the referencePoint as the centerPoint and the radius as half of the wall Segment size ratio * original room width
+ *                 select the endpoint of the temporary Segment that is closer to the other room side Segment (not the chosen one)
+ *                 create a Segment with the previously selected endpoint as the center point and the radiues as half of temp Segemnt's length
+ *                 set the created Segment's endpoint which is closer to the other room side Segment (not the chosen one) as the reference point
  */
