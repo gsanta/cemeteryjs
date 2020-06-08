0b68312248ab35e947d67628db834755fde32d9a
gsanta <gsanta@chemaxon.com>
2020-06-08 22:06:30 +0200

    Axis label added for AxisGizmo.

--- src/plugins/game_viewer/AxisGizmo.ts
+++ src/plugins/game_viewer/AxisGizmo.ts
@@ -1,5 +1,5 @@
-import { Camera, Engine, Matrix, Scene, Vector3 } from 'babylonjs';
-import { AdvancedDynamicTexture, Line } from 'babylonjs-gui';
+import { Camera, Engine, Matrix, Scene, Vector3, Vector2 } from 'babylonjs';
+import { AdvancedDynamicTexture, Line, Button, Control } from 'babylonjs-gui';
 import { Point } from '../../core/geometry/shapes/Point';
 import { Registry } from '../../core/Registry';
 import { Camera3D } from '../common/camera/Camera3D';
@@ -14,7 +14,8 @@ export class AxisGizmo {
     private yLine: Line;
     private zLine: Line;
     private origin3D: Vector3;
-    private origin2D = new Point(40, 40);
+    private origin2D = new Point(40, 60);
+    private axisLen = 45;
 
     constructor(registry: Registry) {
         this.registry = registry;
@@ -23,11 +24,50 @@ export class AxisGizmo {
     awake() {
         this.advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
     
+        this.createButtons();
         this.createXLine();
         this.createYLine();
         this.createZLine();
 
         this.update();
+    }
+
+    private createButtons() {
+        var buttonX = Button.CreateSimpleButton("buttonX", "X");
+        buttonX.width = "15px";
+        buttonX.height = "15px";
+        buttonX.color = "red";
+        buttonX.background = "transparent";
+        buttonX.thickness = 0;
+        buttonX.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+        buttonX.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+        buttonX.top = "40px";
+        buttonX.left = "40px";
+        this.advancedTexture.addControl(buttonX);  
+
+        var buttonY = Button.CreateSimpleButton("buttonY", "Y");
+        buttonY.width = "15px";
+        buttonY.height = "15px";
+        buttonY.color = "green";
+        buttonY.background = "transparent";
+        buttonY.thickness = 0;
+        buttonY.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+        buttonY.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+        buttonY.top = "40px";
+        buttonY.left = "55px";
+        this.advancedTexture.addControl(buttonY);
+
+        var buttonZ = Button.CreateSimpleButton("buttonZ", "Z");
+        buttonZ.width = "15px";
+        buttonZ.height = "15px";
+        buttonZ.color = "blue";
+        buttonZ.background = "transparent";
+        buttonZ.thickness = 0;
+        buttonZ.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+        buttonZ.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+        buttonZ.top = "40px";
+        buttonZ.left = "70px";
+        this.advancedTexture.addControl(buttonZ);
     }
 
     private createXLine() {
@@ -99,6 +139,8 @@ export class AxisGizmo {
             scene.getTransformMatrix(), 
             camera.viewport.toGlobal(engine.getRenderWidth(), engine.getRenderHeight())
         );
+        this.yLine.x1 = this.origin2D.x;
+        this.yLine.y1 = this.origin2D.y;
         this.yAxis = y;
     }
 
@@ -109,31 +151,26 @@ export class AxisGizmo {
             scene.getTransformMatrix(), 
             camera.viewport.toGlobal(engine.getRenderWidth(), engine.getRenderHeight())
         );
+        this.zLine.x1 = this.origin2D.x;
+        this.zLine.y1 = this.origin2D.y;
         this.zAxis = z;
     }
 
     private updateXLine() {
-        const diffX = this.xVector.x - this.origin3D.x; 
-        const diffY = this.xVector.y - this.origin3D.y; 
-        this.xLine.x2 = this.origin2D.x + diffX;
-        this.xLine.y2 = this.origin2D.y + diffY;
+        const vec2 = new Vector2(this.xVector.x - this.origin3D.x, this.xVector.y - this.origin3D.y).normalize().scale(this.axisLen);
+        this.xLine.x2 = this.origin2D.x + vec2.x;
+        this.xLine.y2 = this.origin2D.y + vec2.y;
     }
 
     private updateYLine() {
-        const diffX = this.origin3D.x - this.yAxis.x; 
-        const diffY = this.origin3D.y - this.yAxis.y; 
-        this.yLine.x1 = this.origin2D.x;
-        this.yLine.y1 = this.origin2D.y;
-        this.yLine.x2 = this.origin2D.x + diffX;
-        this.yLine.y2 = this.origin2D.y + diffY;
+        const vec2 = new Vector2(this.origin3D.x - this.yAxis.x, this.origin3D.y - this.yAxis.y).normalize().scale(this.axisLen);
+        this.yLine.x2 = this.origin2D.x + vec2.x;
+        this.yLine.y2 = this.origin2D.y + vec2.y;
     }
 
     private updateZLine() {
-        const diffX = this.origin3D.x - this.zAxis.x; 
-        const diffY = this.origin3D.y - this.zAxis.y; 
-        this.zLine.x1 = this.origin2D.x;
-        this.zLine.y1 = this.origin2D.y;
-        this.zLine.x2 = this.origin2D.x + diffX;
-        this.zLine.y2 = this.origin2D.y + diffY;
+        const vec2 = new Vector2(this.origin3D.x - this.zAxis.x, this.origin3D.y - this.zAxis.y).normalize().scale(this.axisLen);
+        this.zLine.x2 = this.origin2D.x + vec2.x;
+        this.zLine.y2 = this.origin2D.y + vec2.y;
     }
 }
