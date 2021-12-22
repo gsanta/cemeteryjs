e4b40f99d25efd051aba62658707d0e0e969c3bc
Gergely Santa <gergely.santa@bitrise.io>
2021-12-22 01:20:17 +0100

    Movable panel.

--- src/core/ui/components/SidePanel.tsx
+++ src/core/ui/components/SidePanel.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Point } from '../../../utils/geometry/shapes/Point';
 
 interface SideBarProps {
 
 }
 
-const SideBar = (props: SideBarProps): JSX.Element => (
-    <div
-        className="side-panel__header"
-    >
-        Sidepanel
-    </div>
-);
+const SidePanelHeader = (props: SideBarProps): JSX.Element => {
+    const [isMouseDown, setMouseDown] = useState(false);
+    const [cursorPos, setCursorPos] = useState<Point>(null);
+    const [elementPos, setElementPos] = useState<Point>(null);
+
+    function handleMouseMove(e: MouseEvent) {
+        e.preventDefault();
+
+        if (!cursorPos) {
+            return;
+        }
+
+        setCursorPos(new Point(e.clientX, e.clientY));
+
+        const deltaPos = new Point(cursorPos.x - e.clientX, cursorPos.y - e.clientY);
+        setElementPos(new Point(elementPos.x - deltaPos.x, elementPos.y - deltaPos.y));
+    }
+
+    useEffect(() => {
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => window.removeEventListener('mousemove', handleMouseMove);
+
+    }, [isMouseDown]);
+
+    return (
+        <div
+            className="side-panel__header"
+            onMouseDown={() => setMouseDown(true)}
+        >
+            Sidepanel
+        </div>
+    );
+};
 
 const SidePanel = () => (
     <div className="side-panel">
-        <SideBar />
+        <SidePanelHeader />
     </div>
 );
 
